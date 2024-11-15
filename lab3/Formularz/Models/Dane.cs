94c928d891c75a13ea21a277b3b396db2e101edd
wronah <hubertwro7@gmail.com>
2024-11-15 02:14:41 +0100

    add required changes to pass the requirements

--- lab3/Formularz/Models/Dane.cs
+++ lab3/Formularz/Models/Dane.cs
@@ -5,14 +5,42 @@ namespace Formularz.Models
     public class Dane
     {
         [Required(ErrorMessage = "Prosze podaj Imie")]
+        [MinLength(2, ErrorMessage = "Imie musi miec przynajmniej 2 znaki")]
         public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Nazwisko")]
+        [MinLength(2, ErrorMessage = "Nazwisko musi miec przynajmniej 2 znaki")]
+        public string Nazwisko { get; set; }
+
         [Required(ErrorMessage = "Prosze podaj Email")]
-        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        [EmailAddress(ErrorMessage = "Niepoprawny format Email")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Prosze podaj Temat")]
-        public string Temat { get; set; }
-        [Required(ErrorMessage = "Prosze podaj Tresc wiadomosci")]
-        [MinLength(10), MaxLength(50)]
-        public string Tresc { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Haslo")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "Haslo musi miec min 8 znakow, zawierac min jedna duza litere, jedna mala litere i jedna cyfre")]
+        public string Haslo { get; set; }
+
+        [Required(ErrorMessage = "Prosze potwierdz Haslo")]
+        [Compare("Haslo", ErrorMessage = "Hasla musza byc takie same")]
+        public string PotwierdzHaslo { get; set; }
+
+        [Phone(ErrorMessage = "Niepoprawny format Nr Telefonu")]
+        public string? NrTelefonu { get; set; }
+
+        [Range(10, 80, ErrorMessage = "Wiek musi byc pomiedzy 10 a 80")]
+        public int? Wiek { get; set; }
+
+        public string? Miasto { get; set; }
+
+    }
+
+    public enum Miasta
+    {
+        Tarnow = 1,
+        Krakow = 2,
+        Rzeszow = 3,
+        Bochnia = 4,
+        Brzesko = 5
     }
 }
