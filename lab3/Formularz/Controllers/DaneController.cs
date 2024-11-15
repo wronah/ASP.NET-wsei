55811934e55fa9117fb1f8ac595517a35ec24af4
wronah <hubertwro7@gmail.com>
2024-11-15 01:23:23 +0100

    add lab3 with basic structure after completing base code from pdf

---
+++ lab3/Formularz/Controllers/DaneController.cs
@@ -0,0 +1,104 @@
+﻿using Formularz.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Formularz.Controllers
+{
+    public class DaneController : Controller
+    {
+        // GET: DaneController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DaneController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DaneController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DaneController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DaneController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DaneController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DaneController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DaneController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            return View();
+        }
+        public IActionResult Wynik(Dane dane)
+        {
+            return View(dane);
+        }
+    }
+}
---
+++ lab3/Formularz/Models/Dane.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formularz.Models
+{
+    public class Dane
+    {
+        [Required(ErrorMessage = "Prosze podaj Imie")]
+        public string Imie { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Temat")]
+        public string Temat { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Tresc wiadomosci")]
+        [MinLength(10), MaxLength(50)]
+        public string Tresc { get; set; }
+    }
+}
